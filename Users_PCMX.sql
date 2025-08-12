c4b6391893fac3fc2649e0d94dbb73ebe3edc9b0
stwiter <stwiter.19@gmail.com>
2025-08-11 18:00:03 -0600

    Actualizo código SQL

---
+++ Users_PCMX.sql
@@ -0,0 +1,7 @@
+-- Creamos la base de datos con la iformación previamente revisada acorde al DER
+
+CREATE DATABASE Bitacora;
+
+-- Despues de crear la db procedemos a actualizar para que refresque y la usamos para activarla
+
+USE DATABASE Bitacora;
